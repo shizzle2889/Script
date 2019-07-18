55f1d8d1042b0d6bd07314ad4521619976b18066
Shizzle2889 <c.s.murphy2889@gmail.com>
2019-07-18 16:31:03 -0500

    Reorganized, Changed builds DIR

--- AEX.sh
+++ AEX.sh
@@ -1,20 +1,22 @@
-cd /home/shizzle/android/AEX # Set Base Directory
+cd /home/shizzle2889/android/AEX # Set Base Directory
 wget -O .repo/local_manifests/roomservice.xml https://raw.githubusercontent.com/triplr-dev/local_manifests/aex-9.x/master.xml # Pull roomservice.xml
 repo sync -c --force-sync --no-clone-bundle --no-tags # Sync Repository
 . build/envsetup.sh # Set build
 lunch aosp_tblte-userdebug && make aex # Make TBLTE
-mv /home/OUT_DIR/AEX/target/product/tblte/AospExtended-v6.6-tblte-*.zip /home/shizzle/OUT/AEX/TBLTE/ # Move for Upload
-lunch aosp_trlte-userdebug && make aex # Make TRLTE
-mv /home/OUT_DIR/AEX/target/product/trlte/AospExtended-v6.6-trlte-*.zip /home/shizzle/OUT/AEX/TRLTE/ # Move for Upload
-lunch aosp_trlteduos-userdebug && make aex # Make TRLTEDuos
-mv /home/OUT_DIR/AEX/target/product/trlteduos/AospExtended-v6.6-trlteduos-*.zip /home/shizzle/OUT/AEX/TRLTEDuos/ # Move for Upload
-cd /home/shizzle/OUT/AEX/TBLTE/ # Change to OUT directory and Upload
+mv /home/OUT_DIR/AEX/target/product/tblte/AospExtended-v6.6-tblte-*.zip /home/shizzle2889/OUT/AEX/TBLTE/ # Move for Upload
+cd /home/shizzle2889/OUT/AEX/TBLTE/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1WyaI6sou6k92cjwXD9z7cFD2Y27cGXk8 * && s=0 && break || s=$?; done; (exit $s) # Upload TBLTE
-mv /home/shizzle/OUT/AEX/TBLTE/* /home/shizzle/builds/aex_tblte.zip # Move build out of OUT directory
-cd /home/shizzle/OUT/AEX/TRLTE/ # Change to OUT directory and Upload
+mv /home/shizzle2889/OUT/AEX/TBLTE/* /home/shared/shizzle2889/builds/aex_tblte.zip # Move build out of OUT directory
+cd /home/shizzle2889/android/AEX # Set Base Directory
+lunch aosp_trlte-userdebug && make aex # Make TRLTE
+mv /home/OUT_DIR/AEX/target/product/trlte/AospExtended-v6.6-trlte-*.zip /home/shizzle2889/OUT/AEX/TRLTE/ # Move for Upload
+cd /home/shizzle2889/OUT/AEX/TRLTE/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1ifT2ys770gbGmwpX501qPAlMe15SUaxd * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTE
-mv /home/shizzle/OUT/AEX/TRLTE/* /home/shizzle/builds/aex_trlte.zip # Move build out of OUT directory
-cd /home/shizzle/OUT/AEX/TRLTEDuos/ # Change to OUT directory and Upload
+mv /home/shizzle2889/OUT/AEX/TRLTE/* /home/shared/shizzle2889/builds/aex_trlte.zip # Move build out of OUT directory
+cd /home/shizzle2889/android/AEX # Set Base Directory
+lunch aosp_trlteduos-userdebug && make aex # Make TRLTEDuos
+mv /home/OUT_DIR/AEX/target/product/trlteduos/AospExtended-v6.6-trlteduos-*.zip /home/shizzle2889/OUT/AEX/TRLTEDuos/ # Move for Upload
+cd /home/shizzle2889/OUT/AEX/TRLTEDuos/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 13cjqAIUUZ3lTcarpXLEU3mIKG1NOacTC * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTEDuos
-mv /home/shizzle/OUT/AEX/TRLTEDuos/* /home/shizzle/builds/aex_trlteduos.zip # Move build out of OUT directory
-cd /home/shizzle/android/AEX && make clean # Clean OUT_DIR
+mv /home/shizzle2889/OUT/AEX/TRLTEDuos/* /home/shared/shizzle2889/builds/aex_trlteduos.zip # Move build out of OUT directory
+cd /home/shizzle2889/android/AEX && make clean # Clean OUT_DIR
--- AICP.sh
+++ AICP.sh
@@ -4,17 +4,19 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags # Sync Re
 . build/envsetup.sh # Set build
 lunch aicp_tblte-userdebug && make bacon -j$(nproc --all) # Make TBLTE
 mv /home/shizzle2889/OUT_DIR/AICP/target/product/tblte/aicp_tblte_p-14*.zip /home/shizzle2889/OUT/AICP/TBLTE/ # Move for Upload
-lunch aicp_trlte-userdebug && make bacon -j$(nproc --all) # Make TRLTE
-mv /home/shizzle2889/OUT_DIR/AICP/target/product/trlte/aicp_trlte_p-14*.zip /home/shizzle2889/OUT/AICP/TRLTE/ # Move for Upload
-lunch aicp_trlteduos-userdebug && make bacon -j$(nproc --all) # Make TRLTEDuos
-mv /home/shizzle2889/OUT_DIR/AICP/target/product/trlteduos/aicp_trlteduos_p-14*.zip /home/shizzle2889/OUT/AICP/TRLTEDuos/ # Move for Upload
 cd /home/shizzle2889/OUT/AICP/TBLTE/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1F3x_inxEuiZn5X_FD4dUaV79xjtiJDGy * && s=0 && break || s=$?; done; (exit $s) # Upload TBLTE
-mv /home/shizzle2889/OUT/AICP/TBLTE/* /home/shizzle2889/builds/aicp_tblte.zip # Move build out of OUT directory
+mv /home/shizzle2889/OUT/AICP/TBLTE/* /home/shared/shizzle2889/builds/aicp_tblte.zip # Move build out of OUT directory
+cd /home/shizzle2889/android/AICP # Set Base Directory
+lunch aicp_trlte-userdebug && make bacon -j$(nproc --all) # Make TRLTE
+mv /home/shizzle2889/OUT_DIR/AICP/target/product/trlte/aicp_trlte_p-14*.zip /home/shizzle2889/OUT/AICP/TRLTE/ # Move for Upload
 cd /home/shizzle2889/OUT/AICP/TRLTE/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1pVxrwCbj_ife-LUhqoICVOaeoVZOJZkG * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTE
-mv /home/shizzle2889/OUT/AICP/TRLTE/* /home/shizzle2889/builds/aicp_trlte.zip # Move build out of OUT directory
+mv /home/shizzle2889/OUT/AICP/TRLTE/* /home/shared/shizzle2889/builds/aicp_trlte.zip # Move build out of OUT directory
+cd /home/shizzle2889/android/AICP # Set Base Directory
+lunch aicp_trlteduos-userdebug && make bacon -j$(nproc --all) # Make TRLTEDuos
+mv /home/shizzle2889/OUT_DIR/AICP/target/product/trlteduos/aicp_trlteduos_p-14*.zip /home/shizzle2889/OUT/AICP/TRLTEDuos/ # Move for Upload
 cd /home/shizzle2889/OUT/AICP/TRLTEDuos/ # Change to OUT directory and Upload
 for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1HEOMlxXJ-VqURvFs-8bm4PuLX7Zq_CTA * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTEDuos
-mv /home/shizzle2889/OUT/AICP/TRLTEDuos/* /home/shizzle2889/builds/aicp_trlteduos.zip # Move build out of OUT directory
+mv /home/shizzle2889/OUT/AICP/TRLTEDuos/* /home/shared/shizzle2889/builds/aicp_trlteduos.zip # Move build out of OUT directory
 cd /home/shizzle2889/android/AICP && make clean # Clean OUT_DIR
