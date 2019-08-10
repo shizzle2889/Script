bfcd083c0e5052b0932bec97752713c3f690cdb5
Shizzle2889 <c.s.murphy2889@gmail.com>
2019-08-10 11:15:39 -0500

    Xenon Upload Directory

--- Master2.sh
+++ Master2.sh
@@ -71,18 +71,18 @@ repo sync -c -j32 --force-sync --no-clone-bundle --no-tags # Sync Repository
 brunch tblte # Make TBLTE
 mv /home/shared/OUT_DIR/shizzle2889/xenonhd/target/product/tblte/XenonHD-*-Unofficial-tblte.zip /home/shizzle2889/OUT/xenonhd/TBLTE/ # Move for Upload
 cd /home/shizzle2889/OUT/xenonhd/TBLTE/ # Change to OUT directory and Upload
-for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1WyaI6sou6k92cjwXD9z7cFD2Y27cGXk8 * && s=0 && break || s=$?; done; (exit $s) # Upload TBLTE
+for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1-Km3AhrEeF3VuQivwXuI-A8_oeYgZT6g * && s=0 && break || s=$?; done; (exit $s) # Upload TBLTE
 mv /home/shizzle2889/OUT/xenonhd/TBLTE/* /home/shared/shizzle2889/builds/xenonhd_tblte.zip # Move build out of OUT directory
 cd /home/shizzle2889/android/xenonhd # Set Base Directory
 brunch trlte # Make TRLTE
 mv /home/shared/OUT_DIR/shizzle2889/xenonhd/target/product/trlte/XenonHD-*-Unofficial-trlte.zip /home/shizzle2889/OUT/xenonhd/TRLTE/ # Move for Upload
 cd /home/shizzle2889/OUT/xenonhd/TRLTE/ # Change to OUT directory and Upload
-for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1ifT2ys770gbGmwpX501qPAlMe15SUaxd * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTE
+for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1-6_x-vKA73VnL4H3NN_LJYIfJomfTsNL * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTE
 mv /home/shizzle2889/OUT/xenonhd/TRLTE/* /home/shared/shizzle2889/builds/xenonhd_trlte.zip # Move build out of OUT directory
 cd /home/shizzle2889/android/xenonhd # Set Base Directory
 brunch trlteduos # Make TRLTEDuos
 mv /home/shared/OUT_DIR/shizzle2889/xenonhd/target/product/trlteduos/XenonHD-*-Unofficial-trlteduos.zip /home/shizzle2889/OUT/xenonhd/TRLTEDuos/ # Move for Upload
 cd /home/shizzle2889/OUT/xenonhd/TRLTEDuos/ # Change to OUT directory and Upload
-for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 13cjqAIUUZ3lTcarpXLEU3mIKG1NOacTC * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTEDuos
+for i in $(seq 1 50); do [ $i -gt 1 ] ; gdrive upload --parent 1-KsaBsn0lUV2Cy_nr4ZzaBO022xXcezJ * && s=0 && break || s=$?; done; (exit $s) # Upload TRLTEDuos
 mv /home/shizzle2889/OUT/xenonhd/TRLTEDuos/* /home/shared/shizzle2889/builds/xenonhd_trlteduos.zip # Move build out of OUT directory
 cd /home/shizzle2889/android/xenonhd && make clean # Clean OUT_DIR
